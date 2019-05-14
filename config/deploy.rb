3f787dfbac237a223e1f6afcbf7137ad26b9e19c
ofuji <yutanoxxxuta@gmail.com>
2019-05-14 19:42:38 +0900

    edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url,  'git@github.com:YutaOkafuji/freemarket_sample46aw.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.3.1'
+set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/key_pem.pem']  ※例：~/.ssh/key_pem.pem
