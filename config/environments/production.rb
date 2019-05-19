b62961b25d7800856dabf287cd1d9132e8522995
ofuji <yutanoxxxuta@gmail.com>
2019-05-19 19:27:42 +0900

    edit credential.yml.enc

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.require_master_key = true
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
