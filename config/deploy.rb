3916bc9fcfe21aaddd2cb94aa71a13442b7c3eaa
ofuji <yutanoxxxuta@gmail.com>
2019-05-24 19:43:38 +0900

    for error resolve

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,11 +15,26 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+set :linked_files, %w{ config/master.key }
+
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload master.key'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/master.key', "#{shared_path}/config/master.key")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
 
 set :default_env, {
