bbca3d86f09d56345904addd11978b738c151b4b
ofuji <yutanoxxxuta@gmail.com>
2019-06-01 14:39:36 +0900

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :profiles
+  resources :profiles only: [:new, :create]
 
 end
