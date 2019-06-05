3b687f876a891524f3816426bd6c074f0477e7be
ofuji <48400676+YutaOkafuji@users.noreply.github.com>
2019-06-06 08:47:13 +0900

    edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'items#index'
-
-  get 'users/logout' => 'users#logout'
+  resources :users, only: :logout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
