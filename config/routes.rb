03132ba668c4d284c88ad76ec107cc884f4cb479
Doyaface <backwoods.sideman@gmail.com>
2019-06-09 17:51:55 +0900

    Create_logouts_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'items#index'
-  resources :users, only: :logout
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: :index
+  resource :logouts, only: %i[show destroy]
 end
