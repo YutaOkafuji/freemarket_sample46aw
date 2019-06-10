e2dc05e5f7522f34e418084f6c79ada45fa6456a
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-06-10 18:20:22 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+  devise_scope :user do
+    get "sign_in", :to => "devise/sessions#new"
+    get "sign_up", :to => "devise/registrations#new" 
+  end
   root 'items#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
