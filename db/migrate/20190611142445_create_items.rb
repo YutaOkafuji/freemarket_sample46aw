51e26fe6a9714b6bbb6bb386e92a192b568b6134
wtsbee <atsushi@wataseatsushinoMacBook-Air.local>
2019-06-11 23:35:46 +0900

    rake_db:migrate

--- db/migrate/20190611142445_create_items.rb
+++ db/migrate/20190611142445_create_items.rb
@@ -1,7 +1,12 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.string      :name, null: false
+      t.integer     :price, null: false
+      t.text        :description, null: false
+      t.integer     :sale_status, null: false
+      t.integer     :buy_status, null: false
+      t.integer     :user_id
       t.timestamps
     end
   end
