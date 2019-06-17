56e546fd1565a92e56bb777ced6773defe04a0ab
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-06-17 12:51:34 +0900

    add toggle animation to credit new html

--- app/assets/javascripts/credit_page.js
+++ app/assets/javascripts/credit_page.js
@@ -4,4 +4,7 @@ $(document).on('turbolinks:load',function() {
     'pattern': '{{9999}}-{{9999}}-{{9999}}-{{9999}}'
   });
    
+  $('#show-tips-toggle').click(function(){
+    $('.about-seqcode__info').fadeToggle(300);
+  });
 });
