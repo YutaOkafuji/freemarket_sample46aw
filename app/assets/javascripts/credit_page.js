53f647df0f1056a7940af644697cad16d2a7cc02
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-06-19 00:25:43 +0900

    add input_restriction.js plagin

--- app/assets/javascripts/credit_page.js
+++ app/assets/javascripts/credit_page.js
@@ -3,7 +3,9 @@ $(document).on('turbolinks:load',function() {
   $('#credit-input').formatter({
     'pattern': '{{9999}}-{{9999}}-{{9999}}-{{9999}}'
   });
-   
+  
+  $('#credit-input').onlyPhoneNumber();
+
   $('#show-tips-toggle').click(function(){
     $('.about-seqcode__info').fadeToggle(300);
   });
