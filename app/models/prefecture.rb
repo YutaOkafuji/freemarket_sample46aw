9121540166f1896af5d1b1be75401fb1e299854d
ofuji <yutanoxxxuta@gmail.com>
2019-06-20 13:35:16 +0900

    add active_hash

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -1,6 +1,7 @@
 class Prefecture < ActiveHash::Base
   include ActiveHash::Associations
   has_many :items
+  has_many :user_addresses
   self.data = [
       {id: 1, name: '北海道'},
       {id: 2, name: '青森県'},
