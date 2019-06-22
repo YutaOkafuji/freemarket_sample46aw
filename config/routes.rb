e64a8a1ed7ad49aa2e24d0f1131594bd407e3c21
ofuji <yutanoxxxuta@gmail.com>
2019-06-22 14:02:18 +0900

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :logouts, only: %i[show destroy]
+  resource :deals, only: %i[new]
   resources :profiles, only: :new
   resources :users, only: :index
-  resources :deals, only: :new
   # usersのネストにし、indexをshowに修正すること
   resources :credits, only: %i[index new]
   resources :items, only: :new
