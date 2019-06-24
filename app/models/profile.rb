d6149d4f7708b02c3de8311610acd2ce748a4803
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-06-24 17:15:31 +0900

    add Profile model test code

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,10 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  with_options presence: true do
+    validates :family_name
+    validates :first_name
+    validates :family_name_kana
+    validates :first_name_kana
+    validates :user_id
+  end
 end
---
+++ spec/factories/profiles.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :profile do
+    introduction      {"美しい国、日本!!"}
+    birthday          {"19540921"}
+    family_name       {"安倍"}
+    first_name        {"晋三"}
+    family_name_kana  {"abe"}
+    first_name_kana   {"shinzo"}
+  end
+end
