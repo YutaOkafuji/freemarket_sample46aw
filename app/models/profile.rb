ac625803f412d74f17cbd9eb6097e4a4a53b11c6
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-06-25 22:57:58 +0900

    add profile model test code

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,12 @@
 class Profile < ApplicationRecord
+  belongs_to :user
+  with_options presence: true do
+    validates :nickname
+    validates :family_name
+    validates :first_name
+    validates :family_name_kana
+    validates :first_name_kana
+    validates :user_id
+  end
+  validates :nickname, length: {maximum: 20}
 end
