5308d4a8da62d922c755b0e4ed019873dd730c81
ofuji <yutanoxxxuta@gmail.com>
2019-06-26 21:01:44 +0900

    create animation

---
+++ app/assets/javascripts/deals.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('.point-btn').click(function(){
+    let classNames = $('#switch-awesome').attr('class');
+    $('.point-window').slideToggle();
+    if ('fa-chevron-down' == classNames){
+      $('#switch-awesome').replaceWith('<i class="fas fa-chevron-up"></i>');
+    }else{
+      $('#switch-awesome').replaceWith('<i class="fas fa-chevron-down"></i>');
+    }
+
+  })
+})
