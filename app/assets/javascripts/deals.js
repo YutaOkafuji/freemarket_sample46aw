acf6c28a599151653a98b3f40260fa2a529a47eb
ofuji <yutanoxxxuta@gmail.com>
2019-06-28 16:29:51 +0900

    add comment

--- app/assets/javascripts/deals.js
+++ app/assets/javascripts/deals.js
@@ -1,12 +1,13 @@
-$(function(){
-  $('.point-btn').click(function(){
-    let classNames = $('#switch-awesome').attr('class');
-    $('.point-window').slideToggle();
-    if ('fa-chevron-down' == classNames){
-      $('#switch-awesome').replaceWith('<i class="fas fa-chevron-up"></i>');
-    }else{
-      $('#switch-awesome').replaceWith('<i class="fas fa-chevron-down"></i>');
-    }
+// バックエンド作業で行う
+// $(function(){
+//   $('.point-btn').click(function(){
+//     let classNames = $('#switch-awesome').attr('class');
+//     $('.point-window').slideToggle();
+//     if ('fa-chevron-down' == classNames){
+//       $('#switch-awesome').replaceWith('<i class="fas fa-chevron-up"></i>');
+//     }else{
+//       $('#switch-awesome').replaceWith('<i class="fas fa-chevron-down"></i>');
+//     }
 
-  })
-})
+//   })
+// })
