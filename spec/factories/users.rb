118166fbc70e3e96c4d7c1d86b4e2982c4e8b4ef
Doyaface <backwoods.sideman@gmail.com>
2019-06-30 13:41:58 +0900

    Setting-Factory-Bot_modify2

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,9 @@ FactoryBot.define do
     password = Faker::Internet.password(8)
     email { Faker::Internet.email }
     password { password }
-    password_confirmation { password } 
+    password_confirmation { password }
+    sale_status {0}
+    buy_status {0}
     profit { 0 }
     point { 0 }
   end
