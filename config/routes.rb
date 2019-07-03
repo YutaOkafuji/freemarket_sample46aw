76445c01b17386537f80ee1a22f226b2d6327a90
Doyaface <backwoods.sideman@gmail.com>
2019-07-03 19:31:07 +0900

    setting_routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[show] do
     resource  :logouts,        only: %i[show destroy]
-    resource  :profiles,       only: %i[new]
-    resources :items,          only: %i[index show new ]
+    resource  :profiles,       only: %i[new] 
+    resources :items,          only: %i[index show new destroy]
     resource  :user_addresses, only: %i[edit create]
     resource  :credits,        only: %i[show new]
   end
