5906dcce6ee1e0a8666acbf166b6fbdb3e8d870f
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-07-03 15:53:04 +0900

    add sign_up function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,27 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(user_params)
+    @user.profile = Profile.create(profile_params)
     render :create, layout: "second_layout"
   end
   
   def logout
   end
 
+  private
+  def user_params
+    params.permit(:email, :password, :password_confirmation).merge( {avatar: nil, profit: 0, point: 0 } )
+  end
+  
+  def profile_params
+    params.permit(:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana).merge( { birthday: get_birthday } )
+  end
+  
+  def get_birthday
+    birthday = params.permit(:birth_date)
+    birthday = birthday.values
+    birthday= Time.new(birthday[0], birthday[1], birthday[2])
+    birthday.strftime("%Y%m%d")
+  end
 end
