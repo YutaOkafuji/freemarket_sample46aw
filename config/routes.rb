ec7de5b497685afe3f21796df33e347b8c126106
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-07-03 15:51:27 +0900

    change the lines in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'   
-  } 
-  
   root 'items#index'
+  resources :users, only: %i[index new create]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resource :user_addresses, only: %i[edit create new]
   resource :logouts, only: %i[show]
   resources :profiles, only: :new
-  resources :users, only: %i[index new creat]
   # 確認用（users/create）
   get'/users/create' => 'users#create'
   resources :items
   # usersをネストし、indexをshowに修正すること
   resources :credits, only: %i[index new]
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  } 
+
 end
