efe79aa874584666c82aec818cee055aa2829cb3
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-07-06 01:49:54 +0900

    add association at user and user_address

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :profile
+  has_one :profile, dependent: :destroy
+  has_one :user_address, dependent: :destroy
   with_options presence: true do
     validates :profit
     validates :point
