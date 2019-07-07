9f8d460503d8e615fb87f3ae8a0d92af5dbb2c00
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-07-08 02:20:26 +0900

    Update profiles.rb

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,11 +1,17 @@
 FactoryBot.define do
   factory :profile do
-    nickname          {"あべちゃん"}
-    introduction      {"美しい国、日本!!"}
-    birthday          {"19540921"}
-    family_name       {"安倍"}
-    first_name        {"晋三"}
-    family_name_kana  {"abe"}
-    first_name_kana   {"shinzo"}
+    birthday = "#{Faker::Time.between(119.years.ago, Date.today, :all)}".split(" ")[0]
+    nickname          { Faker::Name.name } 
+    introduction      { Faker::Lorem.paragraphs }
+    birthday          { birthday }
+    family_name       { Faker::Name.last_name }
+    first_name        { Faker::Name.first_name }
+    # TODO 日本語のカタカナの名前の文字列を自動生成する機能を実装する
+    family_name_kana  { "アベ" }
+    first_name_kana   { "シンゾウ" }
+  end
+
+  def rand_dt(from = 0.0, to = Time.now)
+    Time.at(from + rand * (to.to_f - from.to_f))
   end
 end
