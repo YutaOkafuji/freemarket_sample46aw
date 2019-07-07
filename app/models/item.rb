192ef1367e86d6c66e5ae72932ae407720224f5e
Doyaface <backwoods.sideman@gmail.com>
2019-07-07 17:26:20 +0900

    item_detail_and_image_association

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to :user
   has_many :item_images, dependent: :destroy
+  has_one :item_detail
+  accepts_nested_attributes_for :item_detail
+  accepts_nested_attributes_for :item_images
 end
