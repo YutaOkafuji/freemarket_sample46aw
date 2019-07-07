a668b855cbfb7948d0b31cff8793249b0e1f4007
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-07-07 14:11:30 +0900

    fix code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
   
   has_one :profile,          dependent: :destroy
   has_one :user_address,     dependent: :destroy
