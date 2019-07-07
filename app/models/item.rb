15315c5ec24a4be3e393e974b5c8b70b66f4eb67
ofuji <yutanoxxxuta@gmail.com>
2019-07-07 16:59:22 +0900

    edit model

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,6 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to :user
   has_many :item_images, dependent: :destroy
+  accepts_nested_attributes_for :item_images
+  accepts_nested_attributes_for :item_details
 end
