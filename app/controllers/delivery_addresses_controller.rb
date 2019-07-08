e83b8215b183d9b911266aa68f2fac523d763a0c
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-07-08 11:19:32 +0900

    Update delivery_addresses_controller.rb

--- app/controllers/delivery_addresses_controller.rb
+++ app/controllers/delivery_addresses_controller.rb
@@ -1,16 +1,16 @@
 class DeliveryAddressesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user, only: [:new, :create]
+  layout "second_layout", only: [:new, :create]
 
   def new
     @delivery_address = DeliveryAddress.new
-    render :new, layout: "second_layout"
   end
 
   def create
-    @user.delivery_address = DeliveryAddress.new(delivery_address_params)
+    @delivery_address = DeliveryAddress.new(delivery_address_params)
     # TODO 単体テストを実装する必要がある
-    if @user.delivery_address.save
+    if @delivery_address.save
       redirect_to new_user_credits_path(user_id: @user.id)
     else
       render action: :new
@@ -19,7 +19,7 @@ class DeliveryAddressesController < ApplicationController
 
   private 
   def delivery_address_params
-    params.require(:delivery_address).permit(:zip_code, :prefecture_id, :city, :street_number, :building, :telephone)
+    params.require(:delivery_address).permit(:zip_code, :prefecture_id, :city, :street_number, :building, :telephone).merge(user_id: current_user.id)
   end
 
   def set_user
