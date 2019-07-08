e77c3a975104fc364fe5a9cc6227fadf6871340a
ofuji <yutanoxxxuta@gmail.com>
2019-07-08 21:56:14 +0900

    edit model

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,8 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :item_details
   has_many :item_images, dependent: :destroy
+  has_one :shipping_origin
   accepts_nested_attributes_for :item_images
   accepts_nested_attributes_for :item_details
+  accepts_nested_attributes_for :shipping_origin
 end
