282cc21ead8c11b327202fb0967105acbbf0ca46
ofuji <yutanoxxxuta@gmail.com>
2019-07-08 20:19:12 +0900

    edit model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to :user
+  has_one :item_details
   has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for :item_images
   accepts_nested_attributes_for :item_details
