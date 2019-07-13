4c4dd5344570e1890a8ab181017ec8ce5ec80363
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-07-13 15:22:29 +0900

    Revert "Merge branch 'master' into create_user_signin_signup_with_google_and_facebook_dev"
    This reverts commit 9c71c2e9cff32b59a95b7064c91111d3dd1ee1a0, reversing
    changes made to 6830808a7d479219938be7a64945dc5a97b4b416.

--- db/migrate/20190713060840_change_column_of_shipping_origins.rb
+++
@@ -1,8 +0,0 @@
-class ChangeColumnOfShippingOrigins < ActiveRecord::Migration[5.2]
-  def up
-    change_column :shipping_origins, :delivery_method_id, :integer , null: true
-  end
-  def down
-    change_column :shipping_origins, :delivery_method_id, :integer , null: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_13_060840) do
+ActiveRecord::Schema.define(version: 2019_07_11_214608) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -128,7 +128,7 @@ ActiveRecord::Schema.define(version: 2019_07_13_060840) do
     t.integer "prefecture_id", null: false
     t.integer "burden_id", null: false
     t.integer "delivery_date_id", null: false
-    t.integer "delivery_method_id"
+    t.integer "delivery_method_id", null: false
     t.index ["item_id"], name: "index_shipping_origins_on_item_id"
   end
 
