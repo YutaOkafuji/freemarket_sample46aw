82acbbed994c86d58d1f2166944edfac80dce1d2
wtsbee <atsushi@wataseatsushinoMacBook-Air.local>
2019-07-13 13:36:43 +0900

    Merge branch 'master' into display_items_details
    # Conflicts:
    #	app/controllers/items_controller.rb
    #	app/models/item.rb
    #	app/models/profile.rb
    #	app/models/user.rb
    #	app/views/items/show.html.haml
    #	config/routes.rb
    #	db/schema.rb

--- app/models/burden.rb
+++ app/models/burden.rb
@@ -1,7 +1,7 @@
 class Burden < ActiveHash::Base
   include ActiveHash::Associations
   self.data = [
-    {id: 1, name: '送料込み(出品者負担)'},
-    {id: 2, name: '着払い(購入者負担)'}
+      {id: 1, name: '送料込み(出品者負担)'},
+      {id: 2, name: '着払い(購入者負担)'}
   ]
 end
--- app/models/shipping_origin.rb
+++ app/models/shipping_origin.rb
@@ -1,5 +1,7 @@
 class ShippingOrigin < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :delivery_date
+  belongs_to_active_hash :delivery_method
   belongs_to_active_hash :burden
   belongs_to_active_hash :prefecture
   belongs_to :item
