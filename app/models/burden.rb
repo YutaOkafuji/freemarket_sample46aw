906f389cdfc7568418560171bf866afb0b67fc53
wtsbee <atsushi@wataseatsushinoMacBook-Air.local>
2019-07-13 12:53:21 +0900

    edit_burden.rb

--- app/models/burden.rb
+++ app/models/burden.rb
@@ -1,8 +1,7 @@
 class Burden < ActiveHash::Base
   include ActiveHash::Associations
   self.data = [
-      {id: 1, name: '1〜2日で発送'},
-      {id: 2, name: '2〜3日で発送'},
-      {id: 3, name: '4〜7日で発送'}
+      {id: 1, name: '送料込み(出品者負担)'},
+      {id: 2, name: '着払い(購入者負担)'}
   ]
 end
