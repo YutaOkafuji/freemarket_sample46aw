5ed983471c0a5c6e627f547d335fb8e6e4611074
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-07-13 14:00:40 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'
-  } 
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   root 'items#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
