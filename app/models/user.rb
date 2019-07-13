8cf7f86b5fdd5b1b8f5e03f887da8e7f36834495
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-07-13 14:01:20 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise  :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable,
+          :omniauthable, omniauth_providers: %i[google_oauth2]
   
   has_one :profile,          dependent: :destroy
   accepts_nested_attributes_for :profile
