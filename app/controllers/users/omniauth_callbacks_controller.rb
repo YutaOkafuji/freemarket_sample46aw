8f3af8d985840f023a69e398b8149e74363c323e
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-07-13 14:02:01 +0900

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
+  def google_oauth2
+  basic_action
+  end
+
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
@@ -28,4 +32,31 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def after_omniauth_failure_path_for(scope)
   #   super(scope)
   # end
+  private
+  # Google認証結果を取得
+  def basic_action
+    @omniauth = request.env["omniauth.auth"]
+    if @omniauth.present?
+      @user = User.where(provider: @omniauth[:provider], uid: @omniauth[:uid]).first
+      binding.pry
+      # アカウントが登録されていなければ登録ページに遷移する
+      unless @user
+        # アカウント情報を作成する
+        @user = User.create(
+          provider: @omniauth.provider,
+          uid: @omniauth.uid,
+          email: @omniauth.info.email,
+          token: @omniauth.credentials.token,
+          password: Devise.friendly_token[0, 20],
+          avatar: nil,
+          profit: 0,
+          point: 0
+        )
+        redirect_to new_user_registration_path(id: @user.id)
+      else
+        sign_in(:user, @user)
+        redirect_to root_path
+      end
+    end
+  end
 end
