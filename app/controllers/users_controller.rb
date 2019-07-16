7d59d0ce5cdb8a7b09b5ab5fb11fc7721459b282
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-07-16 09:33:10 +0900

    remove unnecessary codes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,19 +17,4 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
-
-  def user_params
-    params.permit(:email, :password, :password_confirmation).merge( { avatar: nil, profit: 0, point: 0 } )
-  end
-  
-  def profile_params
-    params.permit(:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana).merge( { birthday: get_birthday } )
-  end
-  
-  def get_birthday
-    birthday = params.permit(:birth_date)
-    birthday = birthday.values
-    birthday= Time.new(birthday[0], birthday[1], birthday[2])
-    birthday.strftime("%Y%m%d")
-  end
 end
