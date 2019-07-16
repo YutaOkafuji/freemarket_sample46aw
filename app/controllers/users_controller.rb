e64d1270a1d331f22523ae8230d85ee753740333
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-07-16 14:29:47 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:new] 
-  before_action :set_user, only: [:show]
+  before_action -> {
+    set_user
+    correct_user?(@user)
+  }, only: %i[show]
+
+  def new
+    render :new, layout: "second_layout"
+  end
 
   def show
+    correct_user?(@user)
     if @user.profile.nil?
       # ToDo もう一度情報を入力されるように促す。
       sign_out(@user)
@@ -10,16 +18,12 @@ class UsersController < ApplicationController
     end
   end
 
-  def new
-    render :new, layout: "second_layout"
-  end
-
-  def index
-    render :new, layout: "second_layout"
-  end
-
   private
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def correct_user?(user)
+    redirect_to root_path, flash: { warning: '不正なアクセス' } unless current_user.id == user.id
   end
 end
