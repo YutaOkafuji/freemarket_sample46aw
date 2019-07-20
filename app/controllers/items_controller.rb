2380ddb3319805d1e86ebdd38ba606860c3aaefd
ofuji <yutanoxxxuta@gmail.com>
2019-07-20 13:13:23 +0900

    edit controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,7 +29,7 @@ class ItemsController < ApplicationController
     if @item.save 
       redirect_to root_path
     else
-      redirect_to new_item_path
+      render :new, layout: 'second_layout'
     end
   end
 
