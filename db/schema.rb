656150e337c2c4f2393f9f41c1bfaa73b1aa04db
Doyaface <backwoods.sideman@gmail.com>
2019-07-20 15:46:48 +0900

    miner_modify_item_list

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2019_07_15_090515) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "parent_id", null: false
